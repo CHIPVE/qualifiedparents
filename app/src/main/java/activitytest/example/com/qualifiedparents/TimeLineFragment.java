f5c14ecf42054315023f7cb2a723ecdd7f40fa93
zhang wei <505308382@qq.com>
2018-12-20 01:00:03 +0800

    为切换底部导航加入了碎片和ViewPaper

---
+++ app/src/main/java/activitytest/example/com/qualifiedparents/TimeLineFragment.java
@@ -0,0 +1,18 @@
+package activitytest.example.com.qualifiedparents;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class  TimeLineFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.timeline_fragment, container, false);
+    }
+
+}
